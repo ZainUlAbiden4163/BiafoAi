a7d4d95e40f8f8afc7ed52b3376214a4e7147550
ZainUlAbiden4163 <shahzain4163@gmail.com>
2026-03-31 03:34:13 +0500

    latest

--- src/paages/Testimonials.jsx
+++ src/paages/Testimonials.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Testimonials() {
   return (
     <section className="flex justify-center items-center bg-primary max-md:py-20 w-full md:h-screen text-white">
-      <main className="flex justify-center items-center bg-cover bg-no-repeat bg-center bg-shadow max-md:py-20 border-white/15 rounded-2xl w-[90%] h-[70%]">
+      <main className="flex justify-center items-center bg-cover bg-no-repeat bg-center bg-shadow max-md:py-20 border-2 border-white/15 rounded-2xl w-[90%] h-[70%]">
         <div className="flex md:flex-row flex-col justify-center items-center gap-3 w-full h-[80%]">
           <div className="inline-block justify-center items-center">
             <Image
